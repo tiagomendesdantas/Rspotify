7031cd2bfcf94754047d5e05c295a935d8ae37d2
Tiago Dantas <t.mendesdantas@gmail.com>
2017-06-11 20:52:13 -0300

    API changes

--- R/searchArtist.R
+++ R/searchArtist.R
@@ -4,22 +4,34 @@
 #'
 #'function to get basic info about a searched Artist
 #'@param artistName Name of the artist
+#'@param token An OAuth token created with \code{spotifyOAuth}.
 #' @examples \dontrun{
 #' ## Example
-#' search<-searchArtist("Foo+fighters")
-#'
+#' ## my_oauth <- spotifyOAuth(app_id="xxxx",client_id="yyyy",client_secret="zzzz")
+#' ## save(my_oauth, file="my_oauth")
+#' ## load("my_oauth")
+#' ## foofighters <- searchArtist("Foo+fighters",token=my_oauth)
 #' }
-#'
-#'
 #'@export
 
 #function to search for a specifc artist
 #note that if you don't provide a specifc name the function will return possible cases that match
 #sorted by popularity
-searchArtist<-function(artistName){
+searchArtist<-function(artistName,token){
   req<-jsonlite::fromJSON(paste0("https://api.spotify.com/v1/search?q=", gsub(' ', '+', artistName),"&type=artist"))
-  artist<-req$artists$items[,c("id","name","popularity","genres","type")]
-  artist$followers<-as.numeric(req$artists$items$followers$total)
-  return(artist)
+
+  req <- httr::GET(paste0("https://api.spotify.com/v1/search?q=", gsub(' ', '+', artistName),"&type=artist"), httr::config(token = token))
+  json1 <- httr::content(req)
+  x<-json1$artists$items
+  display_name=unlist(lapply(x,function(x)x$name))
+  id=unlist(lapply(x,function(x)x$id))
+  popularity<-unlist(lapply(x,function(x)x$popularity))
+  followers<-unlist(lapply(x,function(x)x$followers$total))
+  genres<-unlist(lapply(x,function(x)paste(x$genres,collapse=",")))
+  type<-unlist(lapply(x,function(x)x$type))
+  dados = data.frame(display_name, id, popularity,followers,genres,type, stringsAsFactors = F)
+  return(dados)
 }
+
+
 
