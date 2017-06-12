52c96ba217e15175ad3907f862eca40b361e8b98
Tiago Dantas <t.mendesdantas@gmail.com>
2017-06-11 21:28:40 -0300

    API changes

--- R/getAlbum.R
+++ R/getAlbum.R
@@ -6,8 +6,8 @@
 #'@return get Album from an artist using their ID
 #'@export
 
-getAlbum<-function(id){
-req<-httr::GET(paste0("https://api.spotify.com/v1/albums/",id,"/tracks"))
+getAlbum<-function(id,token){
+req<-httr::GET(paste0("https://api.spotify.com/v1/albums/",id,"/tracks"),httr::config(token = token))
 json1<-httr::content(req)
 json2<-jsonlite::fromJSON(jsonlite::toJSON(json1))$items
 return(json2[,c("id","name","duration_ms","track_number","disc_number","available_markets","preview_url")])
