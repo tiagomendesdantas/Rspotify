b27d4842c5ff4439d88e36c64e7774b199a46de2
Bruna Wundervald <brunadaviesw@gmail.com>
2017-07-08 15:59:38 -0300

    Corrige o texto

--- R/getRelated.R
+++ R/getRelated.R
@@ -3,18 +3,28 @@
 #'
 #'function to get the related artists of an Artist
 #'@param artistName Name of the artist (can use name or Spotify ID of the artist)
+#'@param token An OAuth token created with \code{spotifyOAuth}.
 #'@export
 #'
 #function to get the related artists
 #ps:funtion allows you use the name
-getRelated <-function(artistName){
-  info<-searchArtist(artistName)
+library(plyr)
+
+getRelated <-function(artistName, token){
+  info<-searchArtist(artistName, token = token)
   id<-info$id[1]
-  name<-info$name[1]
-  relatedArtists<-jsonlite::fromJSON(paste0("https://api.spotify.com/v1/artists/",id,"/related-artists"))$artists
-  relatedName<-relatedArtists$name
-  relatedID<-relatedArtists$id
-  followers<-relatedArtists$followers$total
-  popularity<-relatedArtists$popularity
-  return(data.frame(sourceID=id, sourceName=name, names=relatedName,popularity=popularity,followers=followers, id=relatedID,stringsAsFactors=F))
+  name<-info$display_name[1]
+  
+  req <- httr::GET(paste0("https://api.spotify.com/v1/artists/",id,"/related-artists"),httr::config(token = token))
+  json1<-httr::content(req)
+  M <- lapply(json1$artists, "[", 
+              c("name", "id", "popularity", "type" ))
+  N <- lapply(json1$artists, "[[", "followers")
+  N <- lapply(N, "[", "total")
+  
+  relatedArtists <- ldply(M, data.frame)
+  relatedArtists$followers <- ldply(N, data.frame)$total
+
+  return(relatedArtists)
 }
+
